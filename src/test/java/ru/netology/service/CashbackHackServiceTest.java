479320c77f7ecce76fd00336acae030d96668b55
Bragin Aleksey <tech45ru@gmail.com>
2022-04-27 18:36:01 +0500

    Updated CashbackHackServiceTest

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,19 +9,42 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldCalculateCashback() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(service.remain(1000), 1000);
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void shouldCalculateCashback200() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(200);
+        int expected = 800;
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldCalculateCashback500() {
+    public void shouldCalculateCashback400() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(service.remain(1500), 500);
+        int actual = service.remain(400);
+        int expected = 600;
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldCalculateCashback800() {
+    public void shouldCalculateCashback1000() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(service.remain(1200), 800);
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldCalculateCashback1200() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1200);
+        int expected = 800;
+        assertEquals(expected,actual);
     }
 
 }
